38dd0efc4f246993f48c74e9952d06e492d89cb2
Filipe Borges <filipebkc2209@gmail.com>
2024-04-04 14:31:03 -0300

    Added legacy JAR dependency with support of Java Module System

--- add/src/main/java/br/com/filipebores/poc/modularization/add/impl/AddImplementation.java
+++ add/src/main/java/br/com/filipebores/poc/modularization/add/impl/AddImplementation.java
@@ -1,10 +1,15 @@
 package br.com.filipebores.poc.modularization.add.impl;
 
 import br.com.filipebores.poc.modularization.add.api.Add;
+import org.apache.commons.math.stat.descriptive.summary.Sum;
 
 public class AddImplementation implements Add {
     @Override
     public double sum(double num1, double num2) {
-        return num1 + num2;
+        return new Sum().evaluate(
+                new double[]{num1, num2},
+                0,
+                2
+        );
     }
 }
--- add/src/main/java/module-info.java
+++ add/src/main/java/module-info.java
@@ -4,5 +4,7 @@ import br.com.filipebores.poc.modularization.add.impl.AddImplementation;
 module basic.calculator.add {
     exports br.com.filipebores.poc.modularization.add.api;
 
+    requires commons.math;
+
     provides Add with AddImplementation;
 }
